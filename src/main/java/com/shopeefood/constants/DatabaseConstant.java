810d0361a597745fcd79bed3a40d31ab52de084d
ngocdiem138 <89444580+ngocdiem138@users.noreply.github.com>
2022-05-26 20:20:00 +0700

    Added my project

--- src/main/java/com/shopeefood/constants/DatabaseConstant.java
+++ src/main/java/com/shopeefood/constants/DatabaseConstant.java
@@ -4,5 +4,5 @@ public class DatabaseConstant {
     public static String DRIVER = "com.mysql.jdbc.Driver";
     public static String URL = "jdbc:mysql://localhost:3306/shopee_food_db";
     public static String USERNAME = "root";
-    public static String PASSWORD = "123456";
+    public static String PASSWORD = "12345";
 }
